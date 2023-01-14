40b0d8547b57b10c459dac7076509ab117aaf5cf
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-13 22:40:30 -0800

    fix: entry path when `[contenthash]` is used

--- src/entry.ts
+++ src/entry.ts
@@ -15,7 +15,7 @@ function recursive(data: EntryObject, prefix: string = '') {
                 Object.assign(output, recursive(value, `${path}${key}`));
                 continue;
             }
-            else {
+            else if (value.filename.indexOf('[contenthash]') !== -1) {
                 value.filename = `${path}${value.filename}`;
             }
         }
