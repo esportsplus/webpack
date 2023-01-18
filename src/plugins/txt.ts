a7e53603a02507ce488c027773e5fa8a6e63ee6b
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-17 22:47:47 -0800

    feat: convert txt to external file

--- src/plugins/txt.ts
+++ src/plugins/txt.ts
@@ -3,7 +3,10 @@ import { Configuration } from '~/types';
 
 export default (webpack: Configuration) => {
     webpack.module.rules.push({
+        generator: {
+            filename: 'files/[contenthash][ext]'
+        },
         test: /\.txt$/,
-        type: 'asset/source',
+        type: 'asset/resource',
     });
 };
