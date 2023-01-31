92eed6d1fa352f4698d619f2c974b8511015a6bc
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-31 07:40:53 -0800

    fix

--- src/plugins/node/include.ts
+++ src/plugins/node/include.ts
@@ -4,13 +4,11 @@ import path from 'node:path';
 
 
 let delimiter = '/',
-    regex = {
-        at: new RegExp('^@', 'g'),
-        scoped: new RegExp(
-            '@[a-zA-Z0-9][\\w-.]+/[a-zA-Z0-9][\\w-.]+([a-zA-Z0-9./]+)?',
-            'g'
-        )
-    };
+    // Scoped module regex
+    regex = new RegExp(
+        '@[a-zA-Z0-9][\\w-.]+/[a-zA-Z0-9][\\w-.]+([a-zA-Z0-9./]+)?',
+        'g'
+    );
 
 
 function externals(internal: string[] = []): CustomWebpackConfiguration['externals'] {
@@ -31,9 +29,9 @@ function externals(internal: string[] = []): CustomWebpackConfiguration['externa
             let importing = request || '',
                 name = importing.split(delimiter)[0];
 
-            if (regex.scoped.test(importing)) {
+            if (regex.test(importing)) {
                 name = importing.split(delimiter, 2).join(delimiter);
-                regex.scoped.lastIndex = 0;
+                regex.lastIndex = 0;
             }
 
             // Mark module as external
@@ -52,14 +50,12 @@ function read(directory: string) {
     try {
         let dependencies = fs.readdirSync(directory);
 
-        for (let module in dependencies) {
-            if (regex.at.test(module)) {
-                regex.at.lastIndex = 0;
-
+        for (let module of dependencies) {
+            if (module[0] === '@') {
                 try {
                     let dependencies = fs.readdirSync( path.join(directory, module) );
 
-                    for (let dependency in dependencies) {
+                    for (let dependency of dependencies) {
                         list.push(`${module}/${dependency}`);
                     }
 
