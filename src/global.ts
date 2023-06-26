854958cee6beef91a11d49c3b92ccfa4b59c9af4
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-06-26 03:19:42 -0700

    fix: stupid build errors

--- src/global.ts
+++ src/global.ts
@@ -4,3 +4,4 @@ declare module '*.png';
 declare module '*.svg';
 declare module '*.txt';
 declare module '*.webm';
+declare module '*.webp';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import './global';
-
-
 export * as entry from './entry';
 export { default as config } from './config';
 export { default as path } from './path';
