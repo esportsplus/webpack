a173c6ed5b595923dd3b28c67b5539d20400d682
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-06-13 01:01:45 -0700

    fix: svg module type

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,7 +4,12 @@ declare module '*.mp3';
 declare module '*.mp4';
 declare module '*.pdf';
 declare module '*.png';
-declare module '*.svg';
+declare module '*.svg' {
+    symbol: string;
+    title: string;
+    view: string;
+    viewBox: string;
+};
 declare module '*.txt';
 declare module '*.webm';
 declare module '*.webp';
