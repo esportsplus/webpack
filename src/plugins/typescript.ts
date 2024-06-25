97fe95c8b3bfdef352c06912ebb5eb30d4009d61
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-06-24 23:23:31 -0700

    feat: add `${configDir}` support

--- src/plugins/typescript.ts
+++ src/plugins/typescript.ts
@@ -3,9 +3,10 @@ const { EsbuildPlugin } = require('esbuild-loader');
 import { default as TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import { Configuration } from '~/types';
 import esbuild from 'esbuild';
+import path from 'node:path';
 
 
-export default async (config: Configuration, { tsconfig } = { tsconfig: './tsconfig.json'}) => {
+export default async (config: Configuration, { tsconfig } = { tsconfig: './tsconfig.json' }) => {
     config.module.rules.push(
         {
             test: /\.tsx?$/,
@@ -25,10 +26,26 @@ export default async (config: Configuration, { tsconfig } = { tsconfig: './tscon
         })
     );
 
+    let tsc = require( path.resolve(tsconfig) ),
+        url = tsc?.compilerOptions?.baseURL;
+
+    while (tsc?.extends && url === undefined) {
+        tsc = require( path.resolve(tsc.extends) );
+        url = tsc?.compilerOptions?.baseUrl;
+    }
+
+    if (url === undefined || url === '${configDir}') {
+        url = './';
+    }
+    else if (url.startsWith('${configDir}')) {
+        url = url.replace('${configDir}', '.');
+    }
+
     config.resolve.extensions = ['.tsx', '.ts', '.js'];
     config.resolve.fullySpecified = false;
     config.resolve.plugins.push(
         new TsconfigPathsPlugin({
+            baseUrl: url,
             configFile: tsconfig,
             extensions: config.resolve.extensions
         })
