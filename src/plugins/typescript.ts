995168c08e51c13ea38dea1f8ebb66b3605c7371
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-07-10 01:05:57 -0700

    chore: moved `${configDir}` handling into `@esportsplus/tsconfig-paths-webpack-plugin`

--- src/plugins/typescript.ts
+++ src/plugins/typescript.ts
@@ -2,9 +2,7 @@ const { EsbuildPlugin } = require('esbuild-loader');
 
 import { default as TsconfigPathsPlugin } from '@esportsplus/tsconfig-paths-webpack-plugin';
 import { Configuration } from '~/types';
-import { dirname, resolve } from 'node:path';
 import esbuild from 'esbuild';
-import fs from 'node:fs';
 
 
 export default async (config: Configuration, { tsconfig } = { tsconfig: './tsconfig.json' }) => {
@@ -27,44 +25,10 @@ export default async (config: Configuration, { tsconfig } = { tsconfig: './tscon
         })
     );
 
-    let dir = dirname( resolve(tsconfig) ),
-        tsc = require( resolve(tsconfig) ),
-        url = tsc?.compilerOptions?.baseURL;
-
-    while (tsc?.extends && url === undefined) {
-        let p;
-
-        if (tsc.extends.indexOf('${configDir}') !== -1) {
-            p = resolve(dirname(tsconfig), tsc.extends.split('${configDir}').pop());
-        }
-        else if (tsc.extends.startsWith('.')) {
-            p = resolve(dir, tsc.extends);
-        }
-        else {
-            p = resolve(dir, 'node_modules', tsc.extends);
-
-            if (!fs.existsSync(p)) {
-                p = resolve(dirname( resolve(tsconfig) ), 'node_modules', tsc.extends);
-            }
-        }
-
-        dir = dirname(p);
-        tsc = require(p);
-        url = tsc?.compilerOptions?.baseUrl;
-    }
-
-    if (url === undefined || url === '${configDir}') {
-        url = './';
-    }
-    else if (url.startsWith('${configDir}')) {
-        url = url.replace('${configDir}', '.');
-    }
-
     config.resolve.extensions = ['.tsx', '.ts', '.js'];
     config.resolve.fullySpecified = false;
     config.resolve.plugins.push(
         new TsconfigPathsPlugin({
-            baseUrl: url,
             configFile: tsconfig,
             extensions: config.resolve.extensions
         })
