5cae528a4a8346364ad12b88bb50aa9d4f8b443a
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-06-12 18:01:02 -0700

    fix: import error

--- src/plugins/web/polyfill.ts
+++ src/plugins/web/polyfill.ts
@@ -1,12 +1,12 @@
 import { default as NodePolyfillPlugin } from 'node-polyfill-webpack-plugin';
-import { ProvidePlugin } from 'webpack';
+import webpack from 'webpack';
 import { Configuration } from '~/types';
 
 
 const node = (config: Configuration) => {
     config.plugins.push(
         new NodePolyfillPlugin(),
-        new ProvidePlugin({
+        new webpack.ProvidePlugin({
             Buffer: ['buffer', 'Buffer'],
             process: 'process/browser'
         })
